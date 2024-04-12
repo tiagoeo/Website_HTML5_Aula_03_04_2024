57b29396bb0e3b1501a04fceaedba9d99ed82da1
Tiago <99106150+tiagoeo@users.noreply.github.com>
2024-04-12 11:54:55 -0300

    MOD transmitir.php

--- transmitir.php
+++ transmitir.php
@@ -1,6 +1,13 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $email = $_POST['email'];
-        echo($email);
+        $senha = $_POST['senha'];
+
+        if ($email == 'aluno@dominio.com' and $senha == '12345'){
+            echo("Login realizado com sucesso!");
+        }else{
+            echo("Login incorreto!");
+        }
+        
     }
 ?>
