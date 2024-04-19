a4cf8457017958855a05335fddca4e46248029e0
Tiago <99106150+tiagoeo@users.noreply.github.com>
2024-04-19 10:44:43 -0300

    MOD transmitir.php criptografia

--- transmitir.php
+++ transmitir.php
@@ -3,8 +3,10 @@
         $email = $_POST['email'];
         $senha = $_POST['senha'];
 
-        if ($email == 'aluno@dominio.com' and $senha == '12345'){
-            echo("Login realizado com sucesso!");
+        //and $senha == '12345'
+        if ($email == 'aluno@dominio.com' and md5($senha) == 'e10adc3949ba59abbe56e057f20f883e'){
+            echo("Login realizado com sucesso! a senha digitada é: ".$senha);
+            echo("<p>A senha criptografada em md5 é: ".md5($senha));
         }else{
             echo("Login incorreto!");
         }
